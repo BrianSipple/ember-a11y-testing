ff2610ef2661ca243ec383d411402128eed59e39
BrianSipple <Bsipple57@gmail.com>
2016-06-21 19:14:11 -0700

    initial page setup

---
+++ tests/dummy/app/components/x-form.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import layout from '../templates/components/x-form';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: 'form',
+
+  customSelector: 'input',
+});
